f555404b1dd080deff5731e6399c3318457cc265
Aryan Upadhyay <aryan.offc@gmail.com>
2022-08-17 15:42:09 +0530

    Fibonacci

---
+++ src/lec1/Fibonacci.java
@@ -0,0 +1,22 @@
+package lec1;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int term = sc.nextInt();
+        int first = 0;
+        int second = 1;
+
+        while (term > 0) {
+            int sum = first + second;
+            first = second;
+            second = sum;
+            term--;
+        }
+        System.out.println(first);
+
+    }
+}
