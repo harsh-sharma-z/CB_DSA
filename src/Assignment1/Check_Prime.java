7de6719f36d3e9884399ae06ee4184e3e0059ae0
Aryan Upadhyay <aryan.offc@gmail.com>
2022-08-21 15:04:58 +0530

    Check_Prime

---
+++ src/Assignment1/Check_Prime.java
@@ -0,0 +1,20 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Check_Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+        for (int i = 2; i*i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
